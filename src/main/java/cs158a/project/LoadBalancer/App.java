330c0e08f95fb62a59efd90be312cc271a214b5f
Sindhu Pothireddy <sindhu.pothireddy@gmail.com>
2015-04-28 19:26:36 -0700

    Initial commit

---
+++ src/main/java/cs158a/project/LoadBalancer/App.java
@@ -0,0 +1,13 @@
+package cs158a.project.LoadBalancer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/cs158a/project/LoadBalancer/socket_client.java
@@ -0,0 +1,29 @@
+package cs158a.project.LoadBalancer;
+
+import java.io.*;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.*;
+import org.apache.http.util.EntityUtils;
+
+
+public class socket_client {
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws IOException{
+		String host = "http://192.168.56.10/index.html"; //192.168.56.11
+		@SuppressWarnings("resource")
+		HttpClient client = new DefaultHttpClient();
+		HttpGet httpget = new HttpGet(host);
+		HttpResponse response = client.execute(httpget);
+		System.out.println(response.getStatusLine().toString());
+	    HttpEntity entity = response.getEntity();
+	    System.out.println();
+	    System.out.println(EntityUtils.toString(entity));  
+		
+
+
+	}
+}
